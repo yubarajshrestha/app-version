0c88b2b8949fe71c8814ac6e27980fec63b49e47
Yubaraj Shrestha <companion.krish@outlook.com>
2018-12-18 00:22:38 +0545

    nullpointer issue fixed

--- app/src/main/java/np/com/yubarajshrestha/appversion/MainActivity.kt
+++ app/src/main/java/np/com/yubarajshrestha/appversion/MainActivity.kt
@@ -2,11 +2,14 @@ package np.com.yubarajshrestha.appversion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import np.com.yubarajshrestha.version.Version
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val version = Version("", 7)
+        version.current()
     }
 }
