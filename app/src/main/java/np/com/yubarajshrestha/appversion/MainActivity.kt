28d9ea1dddb5801139b8c15abd6fb07521e174b3
Yubaraj Shrestha <companion.krish@outlook.com>
2019-03-13 19:43:06 +0545

    version updated

--- app/src/main/java/np/com/yubarajshrestha/appversion/MainActivity.kt
+++ app/src/main/java/np/com/yubarajshrestha/appversion/MainActivity.kt
@@ -3,6 +3,7 @@ package np.com.yubarajshrestha.appversion
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import np.com.yubarajshrestha.version.Version
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val version = Version("", 7)
-        version.current()
+        val version = Version("https://play.google.com/store/apps/details?id=com.cerdillac.introcreator", 7)
+        Log.d("HELLO", version.current())
+        //version_text.text = version.current().toString()
     }
 }
